1fdf3a2e4dbb8e9d7217f08aed6f2e18f4243813
ale-comi <alecomi95@gmail.com>
2024-02-14 10:36:42 +0100

    creato main

--- inserimento_generi.py
+++ inserimento_generi.py
@@ -1,33 +1,34 @@
 from connessioni import *
 import csv
 
-set_generi = set()
+def inserimento_generi():
+    set_generi = set()
 
-file = open("Dati/elenco_corretto.csv", mode="r", encoding="utf-8", newline="")
+    file = open("Dati/elenco_corretto.csv", mode="r", encoding="utf-8", newline="")
 
-lettore = csv.reader(file, delimiter=",")
+    lettore = csv.reader(file, delimiter=",")
 
-next(lettore)
+    next(lettore)
 
-set_generi = set()
+    set_generi = set()
 
-for riga in lettore:
-    riga = riga[3].strip("\"").split(",")
-    for i in riga:
-        set_generi.add(i)
+    for riga in lettore:
+        riga = riga[3].strip("\"").split(",")
+        for i in riga:
+            set_generi.add(i)
 
-lista_generi = list(set_generi)
+    lista_generi = list(set_generi)
 
-query_insert = """
-INSERT INTO genres(type) 
-VALUES (%s)
-"""
+    query_insert = """
+    INSERT INTO genres(type) 
+    VALUES (%s)
+    """
 
-for i in lista_generi:
-    execute_query_insert(
-        query_insert,
-        (
-            i,
-        ))
+    for i in lista_generi:
+        execute_query_insert(
+            query_insert,
+            (
+                i,
+            ))
 
 
