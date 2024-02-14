635ee22c88a5120b3d1a823f140bce44038f8107
ale-comi <alecomi95@gmail.com>
2024-02-14 11:27:40 +0100

    rating funziona

--- inserimento_user.py
+++ inserimento_user.py
@@ -1,13 +1,15 @@
 from connessioni import *
 import csv
 
-query = """
-INSERT INTO utente(gender, age, work, cap)
-VALUES (%s, %s, %s, %s)
-"""
 
-with open("Dati/users_edit.csv", "r", encoding="utf-8", newline="") as file:
-    lettore = csv.reader(file, delimiter=",")
-    next(lettore)
-    for elem in lettore:
-        execute_query_insert(query, (elem[1], elem[2], elem[4], elem[3]))
+def inserimento_utente():
+    query = """
+    INSERT INTO utente(gender, age, work, cap)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    with open("Dati/users_edit.csv", "r", encoding="utf-8", newline="") as file:
+        lettore = csv.reader(file, delimiter=",")
+        next(lettore)
+        for elem in lettore:
+            execute_query_insert(query, (elem[1], elem[2], elem[4], elem[3]))
