b534ffe172acdc9ae58f413adc435b4fb4ee7e32
ale-comi <alecomi95@gmail.com>
2024-02-15 17:59:28 +0100

    modifiche leggibilità

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import connessioni as conn
 import queries as queries
 import popolamento_tabelle as pt
 
+# drop=True --> viene cancellato e ricreato per intero il database, errore se db non esiste
+# drop=False --> il database non viene modificato, lo crea nuovo se non esiste
+
 try:
     conn.create_db(drop=True)
     queries.create_tables()
